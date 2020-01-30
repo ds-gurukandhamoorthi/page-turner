3ae9da753b299f1c656d7346f418608fa167128d
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-01-30 08:16:07 +0100

    Implement basic page turning

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use enigo::*;
+use std::env;
+use std::process::Command;
+use std::{thread, time};
+
+const WAIT_FOR_WINDOW_ACTIVATING_MS: u64 = 50_u64;
+
+fn main() {
+    let key = env::args().skip(1).next().unwrap();
+    let dur = time::Duration::from_millis(WAIT_FOR_WINDOW_ACTIVATING_MS);
+    let mut enigo = Enigo::new();
+    let ror_args = ["Zathura", "echo", "no-zathura"];
+    Command::new("rp-run-or-raise.sh")
+        .args(&ror_args)
+        .spawn()
+        .expect("Failed to execute ratpoison to switch to Zathura");
+    thread::sleep(dur);
+    match key.as_ref() {
+        "next" => enigo.key_click(Key::Layout('J')),
+        "prior" => enigo.key_click(Key::Layout('K')),
+        _ => eprintln!("Unknown key {}", key),
+    };
+    Command::new("ratpoison")
+        .arg("-c")
+        .arg("focuslast")
+        .spawn()
+        .expect("Failed to execute ratpoison");
+}
